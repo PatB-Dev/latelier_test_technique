390e5ea7cc8e0ffbbc47a953c1d757dc653f743b
code-me-live <patricebelda@gmail.com>
2022-02-14 21:25:44 +0100

    test menu nav

--- web/app/themes/montheme/functions.php
+++ web/app/themes/montheme/functions.php
@@ -3,6 +3,9 @@
 function montheme_supports (){
    add_theme_support('menus');
    register_nav_menu('header', 'En tête');
+//   register_nav_menus(array(
+//       'main'=> 'Accueil',
+//   ));
 }
 
 add_action('after_setup_theme', 'montheme_supports');
--- web/app/themes/montheme/header.php
+++ web/app/themes/montheme/header.php
@@ -11,4 +11,13 @@
 <body>
 <header>
     <img src="<?php echo get_template_directory_uri(); ?>/images/wecare.jpg"/>
+    <nav>
+        <ul>
+            <li>Questions fréquentes</li>
+            <li>Glossaire</li>
+            <li>Certifications</li>
+            <li>Où acheter</li>
+            <li>Nous contacter</li>
+        </ul>
+    </nav>
 </header>
