0cfafee6b86701e15fe81cca1fda13a48cb3721b
code-me-live <patricebelda@gmail.com>
2022-02-16 04:26:51 +0100

    Ajout BG section 1

--- web/app/themes/montheme/index.php
+++ web/app/themes/montheme/index.php
@@ -1,7 +1,9 @@
 <?php get_header() ?>
 
-<h1>Besoin d'aide?</h1>
-<p>Sur ce site vous trouverez des instructions et des réponses à vos questions<br>
-    sur les cartouches et toners.</p>
+<div id="section1">
+    <h1>Besoin d'aide?</h1>
+    <p>Sur ce site vous trouverez des instructions et des réponses à vos questions<br>
+        sur les cartouches et toners.</p>
+</div>
 
 <?php get_footer() ?>
