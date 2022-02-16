a7f7edc8d41f224a032df59c8f4d9c9e94c66a5d
code-me-live <patricebelda@gmail.com>
2022-02-16 03:49:42 +0100

    Alignement paragraphe section 1

--- web/app/themes/montheme/index.php
+++ web/app/themes/montheme/index.php
@@ -1,5 +1,7 @@
 <?php get_header() ?>
 
 <h1>Besoin d'aide?</h1>
+<p>Sur ce site vous trouverez des instructions et des réponses à vos questions<br>
+    sur les cartouches et toners.</p>
 
 <?php get_footer() ?>
